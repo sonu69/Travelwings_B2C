28f768db4417de93062d8c9ceffdd8f50bdc45b2
ttuser <ttuser@DESKTOP-KOKAMRD>
2020-01-11 12:24:18 +0530

    success

--- Travelwings_B2C/src/test/java/demo/Demo_C.java
+++ Travelwings_B2C/src/test/java/demo/Demo_C.java
@@ -1,25 +1,29 @@
 package demo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Demo_C {
 
-	public static void main(String[] args) {
-		WebDriverManager.chromedriver().setup();
-		
-		ChromeOptions op = new ChromeOptions();
-		op.addArguments("--headless");
-		op.setHeadless(false);
-		WebDriver driver = new ChromeDriver(op);
+
+	@Test
+	public void testcase() {
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://test.techtreeit.in/");
 		
 		driver.manage().window().maximize();
 		
-		driver.get("https://test.techtreeit.in/");
-
+		driver.get("https://test.techtreeit.in");
+		
+		driver.findElement(By.xpath("//a[text()='CONTACT US']")).click();
 	}
-
+	
 }
